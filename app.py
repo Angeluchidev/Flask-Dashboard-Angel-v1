6c91988457e879b84871f659730884c4b4bff65b
angeluchidev <angeluchi.dev@gmail.com>
2024-11-29 16:28:03 -0400

    menu + uso de componentes

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
+proyecto = {"titulo": "Angel Template", "intro": "PLantilla Basada en Flask / jinja 2", "logo": "logo.png"}
+
+#menu_items = ['Principal', 'item1', 'item2', 'item3']
+
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template('index.html', proyecto=proyecto, active_route='/')
+
+@app.route('/about')
+def about():
+    return render_template('test.html', proyecto=proyecto, active_route='/about')
+
+@app.route('/contact')
+def contact():
+    return render_template('index.html', proyecto=proyecto, active_route='/contact')
 
 
 if __name__ == '__main__':
